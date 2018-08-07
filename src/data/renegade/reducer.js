3fa9d29197718cb615217ad95d28193b11f761d9
Pierre Segalen <ps@seize.tv>
2018-08-07 18:38:26 +0200

    Inegrated code from @AdrienRedon's comment
    https://github.com/psegalen/stdrenegade/commit/d2f7771b120b1f5cbaf215adce5b82009d275f26#r29986498

--- src/data/renegade/reducer.js
+++ src/data/renegade/reducer.js
@@ -11,14 +11,16 @@ const initialState = {
 const renegadeReducer = (state = initialState, action) => {
     switch (action.type) {
         case renegadeActions.STORE_RENEGADE_DATA:
-            const programs = action.data.programs
             const filteredEvents = action.data.events
                 .sort((a, b) => a.time_start - b.time_start)
                 .filter((value) => new Date().getTime() < value.time_start * 1000)
-            for (let i = 0; i < programs.length; i += 1) {
-                const nextLive = filteredEvents.find((event) => event.program === programs[i].id)
-                programs[i].nextLive = nextLive ? remainingTime(nextLive.time_start * 1000) : ""
-            }
+            const programs = action.data.programs.map((program) => {
+                const nextLive = filteredEvents.find((event) => event.program === program.id)
+                return {
+                    ...program,
+                    nextLive: nextLive ? remainingTime(nextLive.time_start * 1000) : "",
+                }
+            })
             return {
                 ...state,
                 events: action.data.events,
