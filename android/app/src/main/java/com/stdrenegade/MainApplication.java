bdfebb1816df8e86099aee92cec15a0b81abbae5
Pierre Segalen <ps@seize.tv>
2018-12-04 19:33:40 +0100

    Android version is now functional! \o/

--- android/app/src/main/java/com/stdrenegade/MainApplication.java
+++ android/app/src/main/java/com/stdrenegade/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new RNDeviceInfo()
       );
     }
 
